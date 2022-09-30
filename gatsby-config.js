b1ca933b0cb43d64ebff6a0647fc877071ded869
Miracle Jude <judemiraco0@gmail.com>
2022-09-30 02:05:29 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,23 @@
-// require("dotenv").config({
-//   path: `.env`,
-// })
+require("dotenv").config({
+  path: `.env`,
+})
 
-// module.exports = {
-//   siteMetadata: {
-//     title: `webiny photo gallery`,
-//     siteUrl: `https://www.yourdomain.tld`,
-//   },
-//   plugins: [
-//     {
-//       resolve: `gatsby-source-graphql`,
-//       options: {
-//         typeName: "Webiny",
-//         fieldName: "webiny",
-//         url: process.env.WEBINY_API_URL,
-//         headers: {
-//           Authorization: `Bearer ${process.env.WEBINY_API_TOKEN}`
-//         }
-//       }
-//     },
-//   ]
-// }
+module.exports = {
+  siteMetadata: {
+    title: `webiny photo gallery`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: "Webiny",
+        fieldName: "webiny",
+        url: process.env.WEBINY_API_URL,
+        headers: {
+          Authorization: `Bearer ${process.env.WEBINY_API_TOKEN}`
+        }
+      }
+    },
+  ]
+}
